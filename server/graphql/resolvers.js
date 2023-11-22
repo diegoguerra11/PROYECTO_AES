35dc4c688f87697fe7487392693c67a6a657af41
EnriqueCortez30 <108831875+EnriqueCortez30@users.noreply.github.com>
2023-11-22 00:27:38 +0000

    Enrique Delete and Update

--- server/graphql/resolvers.js
+++ server/graphql/resolvers.js
@@ -6,6 +6,8 @@ export const resolvers = {
     Query: {
         hello: () => 'Hello World!',
         projects: async () =>  await Project.find(),
+        project: async (_, { _id}) => await Project.findById(_id),
+        task: async (_, { _id}) => await Task.findById(_id),
         tasks: async () =>await Task.find()
      
     },
@@ -22,6 +24,7 @@ export const resolvers = {
 
             const projectFound = await Project.findById(projectId);
             if(!projectFound) throw new Error ('Project not found');
+            
             const task = new Task({
                 title,
                 projectId,
@@ -29,5 +32,32 @@ export const resolvers = {
             const taskSaved = await task.save();
             return taskSaved;
         },
+        deleteProject: async (_, { _id}) => {
+            const deletedProject = await Project.findByIdAndDelete(_id);
+            if (!deletedProject) throw new Error("Project not found");
+
+            await Task.deleteMany({ projectId: deletedProject._id });
+            
+            return deletedProject;
+        },
+        deleteTask: async (_, { _id}) => {
+            const deletedTask = await Task.findByIdAndDelete(_id);
+            if (!deletedTask) throw new Error("Project not found");
+            return deletedTask;
+        },
+        updateProject: async (_, args) => {
+            const updatedProject = await Project.findByIdAndUpdate(args._id, args, {
+                new: true,    
+            }); 
+            if (!updatedProject) throw new Error("Project not found");
+            return updatedProject;
+        },
+        updateTask: async (_, args) =>{
+            const updateTask = await Task.findByIdAndUpdate(args._id, args, {
+                new: true,
+            });
+            if (!updateTask) throw new Error("Task not found");
+            return updatedTask;
+        }
     },
 };
--- server/graphql/typeDefs.js
+++ server/graphql/typeDefs.js
@@ -4,12 +4,18 @@ export const typeDefs = gql`
     type Query {
         hello: String
         projects: [Project]
+        project(_id: ID!): Project 
+        task(_id: ID!): Task 
         tasks: [Task]
     }
 
     type Mutation {
         createProject(name: String, description: String): Project
         createTask(title: String, projectId: ID): Task
+        updateProject(_id: ID!, name: String!, description: String): Project
+        deleteProject(_id: ID!): Project
+        deleteTask(_id: ID!): Task
+        updateTask(_id: ID!, title: String!, projectID: ID!): Task
     }
 
     type Project {
