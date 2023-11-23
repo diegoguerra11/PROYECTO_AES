b28845f95355cabe11921fd1a500f6c84953b3d0
diegoguerra11 <66859715+diegoguerra11@users.noreply.github.com>
2023-11-23 05:27:54 +0000

    .

--- client/src/graphql/tasks.js
+++ client/src/graphql/tasks.js
@@ -1,7 +1,7 @@
 import { gql } from "@apollo/client";
 
 export const CREATE_TASK = gql`
-mutation ($title: String, projectId: ID){
+mutation ($title: String, $projectId: ID){
     createTask(title: $title, projectId: $projectId){
         title
         project{
